8745feda93bee063e1dd5fa83e8e43123b4112c4
Arshiya207 <arshiya1383.mk@gmail.com>
2024-07-19 15:43:35 +0330

    finish movie page

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 // html content
-function loaderComponent(){
-    return `<div class="loader-container">
+function loaderComponent() {
+  return `<div class="loader-container">
           <div class="loader"></div>
-      </div>`
+      </div>`;
 }
-//functions 
-function removeLoader(element){
-  const loaderContainer=element
+function cardTypeTwo(poster, genres, year, country, title, id) {
+  return `
+    
+        <div class="card-type-two">
+            <div class="type-two-poster-container">
+                <a href="movie.html?q=${id}" class="poster">
+                    <img
+                    src="${poster}"
+                    alt="poster"
+                    class="type-two-img"
+                    />
+                    <div class="type-two-info-overlay">
+                    <span>${genres[0]}-${genres[1] ? genres[1] : ""}</span>
+                    <span>${year}-${country}</span>
+                    </div>
+                </a>
+            </div>
+            <div class="type-two-title">
+            <h3>${title}</h3>
+            </div>
+        </div>
+    `;
+}
+//functions
+function removeLoader(element) {
+  const loaderContainer = element;
 
-  
-      loaderContainer.remove()
-
-  
-
-
+  loaderContainer.remove();
 }
 //variables
 const searchToggleBtn = document.querySelector(".nav-search");
@@ -29,70 +47,51 @@ fetch("https://moviesapi.ir/api/v1/genres")
     data.forEach((genreObj) => {
       filmGenreList.innerHTML += `<li><a href="#">${genreObj.name}</a></li>`;
     });
-   
-  })
+  });
 
 async function genreMovies(genreIndex) {
   const genresApi = "https://moviesapi.ir/api/v1/genres";
-  mainTag.innerHTML+=loaderComponent()
-  let genreCatRes=await fetch(genresApi)
-  let genreCatDataArr=await genreCatRes.json()
-  
-    mainTag.innerHTML += `
+  mainTag.innerHTML += loaderComponent();
+  let genreCatRes = await fetch(genresApi);
+  let genreCatDataArr = await genreCatRes.json();
+
+  mainTag.innerHTML += `
       <div class="card-section">
       <h2 class="card-section-title">${genreCatDataArr[genreIndex].name}</h2>
         <div class="cards-container">
         </div>
       </div>`;
-     const loaderEle =document.querySelector(".loader-container")
+  const loaderEle = document.querySelector(".loader-container");
   const cardsContainers = document.querySelectorAll(".cards-container");
-    removeLoader(loaderEle)
+  removeLoader(loaderEle);
   let genreRes = await fetch(
-    `https://moviesapi.ir/api/v1/genres/${genreIndex+1}/movies?page=1`
+    `https://moviesapi.ir/api/v1/genres/${genreIndex + 1}/movies?page=1`
   );
   let genreData = await genreRes.json();
   let moviesArray = genreData.data;
-  
+
   let numberOfIteration = moviesArray.length >= 7 ? 7 : moviesArray.length;
-  let containerPos=genreIndex
+  let containerPos = genreIndex;
 
   for (let i = 0; i < numberOfIteration; i++) {
-    cardsContainers[containerPos].innerHTML += `
-        <div class="card-type-two">
-            <div class="type-two-poster-container">
-                <a href="#" class="poster">
-                    <img
-                    src="${moviesArray[i].poster}"
-                    alt="poster"
-                    class="type-two-img"
-                    />
-                    <div class="type-two-info-overlay">
-                    <span>${moviesArray[i].genres[0]}-${
-      moviesArray[i].genres[1] ? moviesArray[i].genres[1] : "unknown"
-    }</span>
-                    <span>${moviesArray[i].year}-${
-      moviesArray[i].country
-    }</span>
-                    </div>
-                </a>
-            </div>
-            <div class="type-two-title">
-            <h3>${moviesArray[i].title}</h3>
-            </div>
-        </div>
-    `;
+    cardsContainers[containerPos].innerHTML += cardTypeTwo(
+      moviesArray[i].poster,
+      moviesArray[i].genres,
+      moviesArray[i].year,
+      moviesArray[i].country,
+      moviesArray[i].title,
+      moviesArray[i].id
+    );
   }
 }
 
- async function generateCategory(end,start=0){
+async function generateCategory(end, start = 0) {
+  for (let i = start; i < end; i++) {
+    await genreMovies(i);
+  }
+}
 
-      for(let i=start;i<end;i++){
-          await genreMovies(i)
-      }
- }
-
- generateCategory(3)
-
+generateCategory(3);
 
 // events
 
--- scripts/movieScript.js
+++ scripts/movieScript.js
@@ -1,8 +1,57 @@
+//html content
+function movieAdditionalInfo(plot, actores, writers) {
+  return `
+       <div class="movie-additional-info-container">
+              <div class="movie-plot">
+                    <h3>plot</h3>
+                    <p>${plot}</p>
+              </div>
+              <div class="movie-actores">
+                <h3>actores</h3>
+                <p>${actores}</p>
+              </div>
+              <div class="movie-writers">
+                <h3>writers</h3>
+                <p>${writers}</p>
+              </div>
+        </div> 
+  `;
+}
+function movieInfo(img, poster, title, imdbScore, director, yearOfRelease) {
+  return `
+      <img
+        src="${img}"
+        alt="main page image"
+        class="main-page-img"
+      />
+      <div class="movie-info-container">
+        <div class="movie-info-wrapper">
+          <div class="movie-info-poster">
+            <img
+              src="${poster}"
+              alt="poster"
+              class="movie-info-poster-img"
+            />
+          </div>
+          <div class="movie-info">
+            <h1 class="movie-info-title">${title}</h1>
+            <p class="movie-info-imdb">IMDB ${imdbScore}/10</p>
+            <p class="movie-info-director">${director}</p>
+            <p class="movie-info-year">${yearOfRelease}</p>
+            <button class="movie-info-watch-later tex-icon"><i class="bi bi-floppy"></i> add to your list</button>
+          </div>
+        </div>
+      </div>
+  `;
+}
 //variables
 const searchToggleBtn = document.querySelector(".nav-search");
 const searchBox = document.querySelector(".search-container");
 const searchDismissBtn = document.querySelector(".search-button-dismiss");
 const filmGenreList = document.querySelector("[data-menu-film]");
+const mainEle = document.querySelector(".main");
+const pageHeaderEle = document.querySelector(".page-header");
+const loadingContainer = document.querySelectorAll(".loader-container");
 // api calls
 fetch("https://moviesapi.ir/api/v1/genres")
   .then((res) => res.json())
@@ -10,45 +59,87 @@ fetch("https://moviesapi.ir/api/v1/genres")
     data.forEach((genreObj) => {
       filmGenreList.innerHTML += `<li><a href="#">${genreObj.name}</a></li>`;
     });
-   
-  })
-// events
+  });
+async function getMovieDetails(movieId) {
+  const url = `https://moviesapi.ir/api/v1/movies/${movieId}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const img =
+    "images" in data ? data.images[0] : `images/coffee-beans-7126154.jpg`;
+  loadingContainer[0].classList.add("loader--hidden");
+  loadingContainer[1].classList.add("loader--hidden");
+  loadingContainer[0].addEventListener("transitionend", (e) => {
+    if (e.propertyName == "opacity") {
+      loadingContainer[0].remove();
+      pageHeaderEle.classList.remove("loading");
+      pageHeaderEle.innerHTML += movieInfo(
+        img,
+        data.poster,
+        data.title,
+        data.imdb_rating,
+        data.director,
+        data.released
+      );
+    }
+  });
+
+  loadingContainer[1].addEventListener("transitionend", (e) => {
+    if (e.propertyName == "opacity") {
+      loadingContainer[1].remove();
+      mainEle.innerHTML += movieAdditionalInfo(
+        data.plot,
+        data.actors,
+        data.writer
+      );
+    }
+  });
+}
+// event for page load
+
+window.addEventListener("load", () => {
+  const href = window.location.href;
+  const URLSearch = new URL(href).searchParams;
+  const searchParams = new URLSearchParams(URLSearch).entries();
+  const searchParamsArray = Array.from(searchParams);
+  getMovieDetails(searchParamsArray[0][1]);
+});
+//code block for dropdowns and search box
 document.addEventListener("click", (e) => {
-    let isDropdownBtn = e.target.matches("[data-dropdown-btn]");
-    if (!isDropdownBtn && e.target.closest("[data-dropdown]") != null) return;
-    let currentDropdown = e.target.closest("[data-dropdown]");
-    if (isDropdownBtn) {
-      currentDropdown.classList.toggle("active");
+  let isDropdownBtn = e.target.matches("[data-dropdown-btn]");
+  if (!isDropdownBtn && e.target.closest("[data-dropdown]") != null) return;
+  let currentDropdown = e.target.closest("[data-dropdown]");
+  if (isDropdownBtn) {
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown]").forEach((dr) => {
+    if (dr == currentDropdown) return;
+
+    dr.classList.remove("active");
+  });
+  let isSearchBtn = e.target.matches("[data-search-btn]");
+  if (
+    (!isSearchBtn && e.target.closest("[data-search-box]") != null) ||
+    e.target.closest("[data-search-form]") != null
+  )
+    return;
+  if (!isSearchBtn) {
+    let searchBoxIsActive = document
+      .querySelector(".search-container")
+      .classList.contains("active");
+    if (searchBoxIsActive == true) {
+      document.querySelector(".search-container").classList.remove("active");
     }
-  
-    document.querySelectorAll("[data-dropdown]").forEach((dr) => {
-      if (dr == currentDropdown) return;
-  
-      dr.classList.remove("active");
-    });
-    let isSearchBtn = e.target.matches("[data-search-btn]");
-    if (
-      (!isSearchBtn && e.target.closest("[data-search-box]") != null) ||
-      e.target.closest("[data-search-form]") != null
-    )
-      return;
-    if (!isSearchBtn) {
-      let searchBoxIsActive = document
-        .querySelector(".search-container")
-        .classList.contains("active");
-      if (searchBoxIsActive == true) {
-        document.querySelector(".search-container").classList.remove("active");
-      }
-    }
+  }
+});
+searchToggleBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchBox.classList.toggle("active");
+  document.querySelectorAll("[data-dropdown]").forEach((dr) => {
+    dr.classList.remove("active");
   });
-  searchToggleBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    searchBox.classList.toggle("active");
-    document.querySelectorAll("[data-dropdown]").forEach((dr) => {
-      dr.classList.remove("active");
-    });
-  });
-  searchDismissBtn.addEventListener("click", () => {
-    searchBox.classList.remove("active");
-  });
-  
+});
+searchDismissBtn.addEventListener("click", () => {
+  searchBox.classList.remove("active");
+});
+//code block for dropdowns and search box
